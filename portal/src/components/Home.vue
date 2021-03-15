e736f5e87a8bd74873d6e7a9b5148330184a7752
Shqipdon Drenica <sd45762@ubt-uni.net>
2021-03-15 23:40:56 +0100

    First Commit

---
+++ portal/src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class = "logo">
+
+
+        </div>
+        <div class="menu">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Lajmet</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        <h2>Hello world - from  Home Component</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+<style scope>
+    .logo{
+        float:left;
+    }
+    .menu{
+        float:right;
+    }
+   .menu ul {
+       list-style-type: none;
+    }
+     .menu ul li {
+         display: inline;
+    }
+
+</style>
